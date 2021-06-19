7be61350e025e3a6ff94acdee85542d99405e761
Ygor Lima <ygorcross@gmail.com>
2021-06-19 02:09:23 -0300

    updated viewmodel unit tests

--- app/src/test/java/com/cornershop/counterstest/counters/viewmodel/CountersViewModelUnitTest.kt
+++ app/src/test/java/com/cornershop/counterstest/counters/viewmodel/CountersViewModelUnitTest.kt
@@ -6,8 +6,11 @@ import com.cornershop.counterstest.counter.model.Counter
 import com.cornershop.counterstest.counter.viewmodel.CountersViewModel
 import com.cornershop.counterstest.utils.data.NetworkError
 import com.cornershop.counterstest.utils.data.StateMachineEvent
+import com.cornershop.counterstest.utils.data.UnauthorizedError
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import io.mockk.verifySequence
 import io.reactivex.Single
 import io.reactivex.subjects.SingleSubject
 import junit.framework.Assert.assertEquals
@@ -33,7 +36,7 @@ class CountersViewModelUnitTest : BaseViewModelTests() {
         // given
         val publish = SingleSubject.create<List<Counter>>()
         val expected = listOf(Counter("id1", "title1", 1))
-        stubInteractorCounters(publish)
+        stubInteractorGetCounters(publish)
 
         // when
         viewModel.getCounters()
@@ -53,8 +56,8 @@ class CountersViewModelUnitTest : BaseViewModelTests() {
     fun `should throw error when get counters without success`() {
         // given
         val publish = SingleSubject.create<List<Counter>>()
-        val expectedException = NetworkError
-        stubInteractorCounters(publish)
+        val expectedException = UnauthorizedError
+        stubInteractorGetCounters(publish)
 
         // when
         viewModel.getCounters()
@@ -68,9 +71,46 @@ class CountersViewModelUnitTest : BaseViewModelTests() {
         val result = viewModel.countersState.value as StateMachineEvent.Failure
         assertThat(viewModel.countersState.value).isExactlyInstanceOf(result.javaClass)
         assertEquals(result.exception, expectedException)
+        verify { mockInteractor.getCounters(fetchFromRemote = true) }
     }
 
-    private fun stubInteractorCounters(counters: Single<List<Counter>>) {
+    @Test
+    fun `should fetch data from local store when throw network error trying to get counters`() {
+        // given
+        val publishFromRemote = SingleSubject.create<List<Counter>>()
+        val publishFromLocal = SingleSubject.create<List<Counter>>()
+        val expected = listOf(Counter("id1", "title1", 1))
+        val expectedException = NetworkError
+        stubInteractorGetCounters(publishFromRemote, fetchFromRemote = true)
+        stubInteractorGetCounters(publishFromLocal, fetchFromRemote = false)
+
+        // when
+        viewModel.getCounters()
+
+        assertThat(viewModel.countersState.value)
+            .isExactlyInstanceOf(StateMachineEvent.Start.javaClass)
+
+        publishFromRemote.onError(expectedException)
+        publishFromLocal.onSuccess(expected)
+
+        // then
+        val result = viewModel.countersState.value as StateMachineEvent.Success<List<Counter>>
+        assertThat(viewModel.countersState.value).isExactlyInstanceOf(result.javaClass)
+        assertEquals(result.value, expected)
+        verifySequence {
+            mockInteractor.getCounters(fetchFromRemote = true)
+            mockInteractor.getCounters(fetchFromRemote = false)
+        }
+    }
+
+    private fun stubInteractorGetCounters(counters: Single<List<Counter>>) {
         every { mockInteractor.getCounters(any()) } returns counters
+    }
+
+    private fun stubInteractorGetCounters(
+        counters: Single<List<Counter>>,
+        fetchFromRemote: Boolean
+    ) {
+        every { mockInteractor.getCounters(fetchFromRemote) } returns counters
     }
 }
